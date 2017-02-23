cec1498f845c0a2447e5246f747ffe14b3212b60
AlbertoParravicini <albyratta@gmail.com>
2017-02-24 00:07:24 +0100

    Added parallax scrolling, updated performances

--- Project/src/website/js/main-bootstrap.js
+++ Project/src/website/js/main-bootstrap.js
@@ -17,28 +17,28 @@ $(document).ready(function () {
     }
     /* Check the location of each desired element */
     else {
-      $(window).on("scroll", function () {
-        $('.fade-in-object').each(function (i) {
-          var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-          var bottom_of_window = $(window).scrollTop() + $(window).height();
 
-          /* Adjust the "300" to either have a delay or that the content starts fading a bit before you reach it  */
-          bottom_of_window = bottom_of_window + 300;
+      $('.fade-in-object').each(function (i) {
+        var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+        var bottom_of_window = $(window).scrollTop() + $(window).height();
 
-          /* If the object is completely visible in the window, fade it it */
-          if (bottom_of_window > bottom_of_object) {
-            if ($(this).hasClass("fade-on")) {
-              $(this).animate({ 'opacity': '1' }, 2000);
-            }
-            else if ($(this).hasClass("fade-left")) {
-              $(this).addClass('animated fadeInLeft');
-            }
-            else if ($(this).hasClass("fade-right")) {
-              $(this).addClass('animated fadeInRight');
-            }
+        /* Adjust the "300" to either have a delay or that the content starts fading a bit before you reach it  */
+        bottom_of_window = bottom_of_window + 300;
+
+        /* If the object is completely visible in the window, fade it it */
+        if (bottom_of_window > bottom_of_object) {
+          if ($(this).hasClass("fade-on")) {
+            $(this).animate({ 'opacity': '1' }, 2000);
           }
-        });
-      })
+          else if ($(this).hasClass("fade-left")) {
+            $(this).addClass('animated fadeInLeft');
+          }
+          else if ($(this).hasClass("fade-right")) {
+            $(this).addClass('animated fadeInRight');
+          }
+        }
+      });
+
     }
   });
 
@@ -61,6 +61,9 @@ $(document).ready(function () {
     $(".splash-arrow").css({ 'opacity': '1' });
   });
 
+  $(".fade-in-object").on("webkitAnimationEnd", function (e) {
+    $(this).removeClass('fade-in-object');
+  });
 
   $('.interactive-canvas').bind('mouseenter', function (e) {
     disableScroll();
